66e3cf54a8e0cdc0d1d6d0f7ca62bf949afbbbd6
Link4Electronics <link4electronics@gmail.com>
2026-04-06 04:37:56 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,9 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    libdecor \
+    openra
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
