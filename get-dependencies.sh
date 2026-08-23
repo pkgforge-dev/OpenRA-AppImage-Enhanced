5d4acdb65dd7cc5b5cb14d6ae79fac832d3e0135
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:06:12 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,11 +7,10 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libdecor       \
     openra         \
     pipewire-audio \
     pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
